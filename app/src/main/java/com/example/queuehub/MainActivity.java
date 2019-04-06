0c948cd74e81e780bd84fd6d5330c3e090276204
Zach Pole <42594071+ZachPole@users.noreply.github.com>
2019-04-06 00:30:17 -0700

    pushing seekbar fix

--- app/src/main/java/com/example/queuehub/MainActivity.java
+++ app/src/main/java/com/example/queuehub/MainActivity.java
@@ -1,10 +1,13 @@
 package com.example.queuehub;
 
 import android.Manifest;
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.media.MediaPlayer;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
@@ -56,6 +59,7 @@ public class MainActivity extends AppCompatActivity {
     TextView elapsedTime;
     TextView remainingTime;
     ProgressBar progressBar;
+    int totalTime;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -118,7 +122,47 @@ public class MainActivity extends AppCompatActivity {
                             player.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
                                 @Override
                                 public void onPrepared(MediaPlayer mp) {
-                                    player.start();
+                                    //adding seek bar in here
+                                    totalTime = player.getDuration();
+                                    seekBar.setMax(totalTime);
+                                    //seek bar
+                                    seekBar.setOnSeekBarChangeListener(
+                                            new SeekBar.OnSeekBarChangeListener() {
+                                                @Override
+                                                public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                                                    if(fromUser){
+                                                        player.seekTo(progress);
+                                                        seekBar.setProgress(progress);
+                                                    }
+                                                }
+
+                                                @Override
+                                                public void onStartTrackingTouch(SeekBar seekBar) {
+
+                                                }
+
+                                                @Override
+                                                public void onStopTrackingTouch(SeekBar seekBar) {
+
+                                                }
+                                            }
+                                    );
+
+                                    new Thread(new Runnable() {
+                                        @Override
+                                        public void run() {
+                                            while(player != null) {
+                                                try{
+                                                    Message msg = new Message();
+                                                    msg.what = player.getCurrentPosition();
+                                                    handler.sendMessage(msg);
+                                                    Thread.sleep(1000);
+                                                } catch (InterruptedException e) {}
+                                            }
+                                        }
+                                    }).start();
+                                    //end seek bar addition
+                                    //player.start();
                                     btnPlay.setBackgroundResource(R.drawable.stop);
                                     btnPlay.setOnClickListener(new View.OnClickListener() {
                                         @Override
@@ -162,6 +206,35 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
+
+    //seek bar helper functions
+    @SuppressLint("HandlerLeak")
+    private Handler handler = new Handler(){
+        @Override
+        public void handleMessage(Message msg){
+            int currentPosition = msg.what;
+            seekBar.setProgress(currentPosition);
+
+            String elapsed = createTimeLabel(currentPosition);
+            elapsedTime.setText(elapsed);
+
+            String remaining = createTimeLabel(totalTime - currentPosition);
+            remainingTime.setText("- " + remaining);
+        }
+    };
+
+    public String createTimeLabel(int time){
+        String timeLabel = "";
+        int min = time / 1000 / 60;
+        int sec = time / 1000 % 60;
+
+        timeLabel = min + ":";
+        if(sec < 10) timeLabel += "0";
+        timeLabel += sec;
+
+        return timeLabel;
+    }
+    //end seek bar helper functions
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
